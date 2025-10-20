c16b0a9cdf5ecd5f066cccef6d33382123928d62
Tabo462 <tabopersonal@gmail.com>
2025-10-19 20:13:15 -0600

    Fimal

--- index.py
+++
@@ -1,6 +0,0 @@
-from Panel_de_Inicio import Manager
-
-if __name__ == "__main__":
-    # Ejecuta la aplicación GUI sin abrir una consola (usa pythonw.exe o doble clic en .pyw)
-    app = Manager()
-    app.mainloop()
